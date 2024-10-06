35d4229c2aa11e21be1509ad40a15b474d868e4f
Tarun-surendra <tarunsurendramothukuru@gmail.com>
2024-10-06 14:25:14 -0400

    week5 changes

---
+++ frontend/src/components/AppointmentSection.js
@@ -0,0 +1,14 @@
+// src/components/AppointmentSection.js
+import React from 'react';
+
+const AppointmentSection = () => {
+    return (
+        <div className="passage">
+            <h3>Book An Appointment:</h3>
+            <img src="img/BookAnAppointment.jpg" className="float-end custom-img" alt="Book An Appointment" />
+            <p>Booking an appointment at "JANATHA GARAGE" ensures convenient, prioritized service with no waiting in line...</p>
+        </div>
+    );
+}
+
+export default AppointmentSection;
---
+++ frontend/src/components/ServiceSection.js
@@ -0,0 +1,14 @@
+// src/components/ServiceSection.js
+import React from 'react';
+
+const ServiceSection = () => {
+    return (
+        <div className="passage">
+            <h3>Service:</h3>
+            <img src="img/Service.jpg" className="float-start custom-img" alt="Service" />
+            <p>Our garage is dedicated to providing exceptional automotive services tailored to meet the unique needs...</p>
+        </div>
+    );
+}
+
+export default ServiceSection;
